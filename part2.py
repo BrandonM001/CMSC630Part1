867e86dafe9a0282ef3d9489f6c11a7c296a1271
Brandon M <43420985+BrandonM001@users.noreply.github.com>
2023-04-14 04:12:45 -0400

    PDF file was corrupted, re-committing to be safe

--- part2.py
+++ part2.py
@@ -9,7 +9,7 @@
 #https://en.wikipedia.org/wiki/Prewitt_operator
 #https://www.analyticsvidhya.com/blog/2022/02/k-fold-cross-validation-technique-and-its-essentials/
 
-from part1 import *
+from part1 import * #need monoColor from last part
 import math
 import sys #used to get inputFile
 from PIL import Image #used to open Images
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plot
 import numpy as np #used to convert images to numbers for processing
 import shlex #used to help parse input
 import random
-import time
+import time #used to get time metrics
 
 
 if len(sys.argv) < 2: #make sure we have an input file
@@ -42,12 +42,12 @@ def main():  # main function
             filename = parts[0]# name/path of image file
             names.append(filename)
             img = openImg(filename) #function from part1
-            print(filename)
-            timings = time.time()
+            print("Running: " + filename)
+            timings = time.time() #take starting time
             monoImg = monoColor(img, parts[1]) #converts color images to selected single color
             #monoColor from part1
-            imageTime.append(time.time() - timings)
-            timings = time.time()
+            imageTime.append(time.time() - timings) #measures how long the last function ran
+            timings = time.time() #reset timer
             imageARY.append(monoImg)
             #imageTime.append(imageTime)
             sobel = sobelFilter(monoImg) #apply sobel operator
@@ -64,42 +64,40 @@ def main():  # main function
             imageTime.append(time.time() - timings)
             timeCount.append(imageTime)
             Image.fromarray(monoImg, 'RGB').show() #print original image
-            Image.fromarray(sobel, 'RGB').show()
-            Image.fromarray(dilate, 'RGB').show()
-            Image.fromarray(erosion, 'RGB').show()
+            Image.fromarray(sobel, 'RGB').show() #print edges
+            Image.fromarray(dilate, 'RGB').show() #print dilated image
+            Image.fromarray(erosion, 'RGB').show() #print eroded image
             #plot.bar(range(256), histoThres)
-            Image.fromarray(cluster, 'RGB').show()
-        for pict in range(0, numImages):
+            Image.fromarray(cluster, 'RGB').show() #print foreground of image
+        for pict in range(0, numImages):#display how long each function took for each image
             print(names[pict])
-            print("Grayscale took " + str(timeCount[pict][0]) + "seconds")
-            print("Sobel took " + str(timeCount[pict][1]) + "seconds")
-            print("Dilation took " + str(timeCount[pict][2]) + "seconds")
-            print("Erosion took " + str(timeCount[pict][3]) + "seconds")
-            print("Clustering took " + str(timeCount[pict][4]) + "seconds")
+            print("Grayscale took " + str(timeCount[pict][0]) + " seconds")
+            print("Sobel took " + str(timeCount[pict][1]) + " seconds")
+            print("Dilation took " + str(timeCount[pict][2]) + " seconds")
+            print("Erosion took " + str(timeCount[pict][3]) + " seconds")
+            print("Clustering took " + str(timeCount[pict][4]) + " seconds")
             img = imageARY[pict]
         return 0
 
 def sobelFilter(img):
     xAxis = img.shape[0]#get general size info
     yAxis = img.shape[1]
-    # Define the sobel kernels
+    #define sobel kernels
     kernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     kernelM = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
     kernelY = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
     gradX = np.array(img) #make kernels for each dimension
     gradY = np.array(img)
-    kerX = kernelX.shape[0]
+    kerX = kernelX.shape[0]#get kernel size
     kerY = kernelY.shape[1]
     copy = np.array(img)
     # Apply the kernels to the image
     for i in range(1, xAxis-1):
-        row = []
-        for j in range(1, yAxis-1):
-            colm = []
+        for j in range(1, yAxis-1): #loop through each pixel in image
             region = np.array(img)
             xImg = np.sum(np.sum(kernelX * img[i-1:i+2, j-1:j+2]))
             yImg = np.sum(np.sum(kernelY * img[i-1:i+2, j-1:j+2]))
-            #these get 3x3 blocks of pixels around the pixel at i, j
+            #"img[i-1:i+2, j-1:j+2]" gets 3x3 blocks of pixels around the pixel at i, j
             #part1 would've been simplier if I knew i could do this in python
             copy[i, j] = (np.sqrt(np.square(xImg) + np.square(yImg)))
     return copy
@@ -117,49 +115,54 @@ def dilationErosion(img, kernel, dilationOrErosion=True):
     kerX = int(xAxis2/2)
     kerY = int(yAxis2/2)
     padded = np.zeros([xAxis1 + kerX, yAxis1 + kerY, 3]) #add padding to prevent out of bounds errors
-    padded[kerX - 1 : -1, kerY - 1 : -1] = img #copy original image
-    kern = np.zeros([xAxis2, yAxis2, 3])
+    padded[kerX - 1 : -1, kerY - 1 : -1] = img #copy original image to new image
+    kern = np.zeros([xAxis2, yAxis2, 3]) #make 3d array of 0s to convert 2D kernel
     for k in range(xAxis2):
         for l in range(yAxis2):
             num = kernel[k, l]
-            kern[k, l] = [num, num, num] #convert kernel to RGB
+            kern[k, l] = [num, num, num] #convert kernel to RGB format
     kernel = kern
     for i in range(kerX, xAxis1):
-        for j in range(kerY, yAxis1):
+        for j in range(kerY, yAxis1):#iterate through pixel in image
             region = padded[(i-kerX):(i+kerX+1), (j-kerY):(j+kerY+1)]
             #grab 3x3 area of pixels around current pixel
             val = 0
             bigPix = region * kernel
             if dilationOrErosion: #only difference is min/max, so I put both into 1 function
-                bigPix = bigPix[bigPix != 0]
+                bigPix = bigPix[bigPix != 0] #eliminate 0s from area
+                #after union with kernel, since at least 1 pixel is guranteed to be 0
+                #it makes all pixels in image black
                 #print(bigPix)
                 val = int(np.min(bigPix)) #apply dilation
 
             else:
                 val = int(np.max(bigPix)) #apply erosion
-            copy[i][j] = val
+            copy[i][j] = val #update pixel value
     return copy
 def histogram_thresholding(image):#couldn't get this to compile, so i removed it
     # Compute the histogram of pixel intensities
     return 0
 def clustering(img, clusters=3, iters=3): #attempted from Lecture 12 slide 11
     copy = np.array(img)
-    xAxis = img.shape[0]
+    xAxis = img.shape[0] #get general image info
     yAxis = img.shape[1]
     #Initialize: Pick K random points as cluster centers
     randK = np.random.randint(0, 255, size=(clusters, 3))
     #print(randK)
-    for iter in range(iters):
-        tempIMG = np.array(img)
+    for iter in range(iters): #amount of times to run k-means
+        #your supposed to have k-means exit when it stops seeing significant changes
+        # but I didn't get that to work
+        tempIMG = np.array(img) #copy array
+        z = 0
         for i in range(xAxis):
             for j in range(yAxis):
                 #Assign data points to closest cluster center.
                 pix = tempIMG[i][j]
-                diff = np.sqrt(np.sum(np.square(randK-pix),axis=1))
-                c = np.argmin(diff)
-                tempIMG[i][j] = randK[c]
+                num = np.sqrt(np.sum(np.square(randK-pix),axis=1))
+                z = np.argmin(num)
+                tempIMG[i][j] = randK[z]
                 #Change the cluster center to the average of its assigned points
-        randK[iter] = np.mean(tempIMG)
+        randK[z] = np.mean(tempIMG)
     return tempIMG
 
 
